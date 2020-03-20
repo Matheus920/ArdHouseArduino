500cf0f4196ffe667bf6903ee05ac32492f79b57
Igor Augusto <61761206+IgorSantoss@users.noreply.github.com>
2020-03-20 17:17:22 -0300

    adição dos comentários

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,45 +1,44 @@
 #include <SPI.h>
 #include <UIPEthernet.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; 
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; //definiçcoes de endereço
 byte ip[] = { 192, 168, 15, 175 }; 
 EthernetServer server(80); 
 
 int ledPin = 4; 
 
-String readString = String(30); 
-int status = 0; 
+String readString = String(30); //declaração da string que armazena o http
 
 void setup(){
-  Ethernet.begin(mac, ip); 
-  server.begin(); 
+  Ethernet.begin(mac, ip); //inicia a comunicação com o modulo
+  server.begin(); //inicia o servidor
   pinMode(ledPin, OUTPUT); 
   digitalWrite(ledPin, LOW); 
   }
 
 void loop(){
-    EthernetClient client = server.available();
-    if(client){
-        while(client.connected()){
-            if(client.available()){
-                char c = client.read();
+    EthernetClient client = server.available(); 
+    if(client){ //caso exista um cliente:
+        while(client.connected()){ // enquanto estiver conectado
+            if(client.available()){ //se o cliente estiver disponivel
+                char c = client.read(); //le os caracteres da requisição
                 if(readString.length() < 100){
-                    readString += c;
+                    readString += c; // transfere os caracteres para string
                 }
 
-                if(c == '\n'){
-                    if(readString.indexOf("?")<0){
+                if(c == '\n'){//se encontrar o '\n' eh o final da requisição
+                    if(readString.indexOf("?")<0){ //verifica se existe o caractere '?'
 
                     }else{
-                        if(readString.indexOf("ledParam=1")>0){
+                        if(readString.indexOf("ledParam=1")>0){ //se existir a string "ledParam=1" liga o led
                             digitalWrite(ledPin,HIGH);
                         } else{
                             digitalWrite(ledPin, LOW);
                         }
-                         client.println("HTTP/1.1 200 OK");                       
+                         client.println("HTTP/1.1 200 OK"); //retorna um OK                   
                     }
-                    readString="";
-                    client.stop();
+                    readString=""; //limpa a string para novas requisições
+                    client.stop(); // desliga o cliente
                 }
             }
         }
