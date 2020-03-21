f3b98794f1c153064315fd4b2df2bd278eaff779
Igor Augusto <61761206+IgorSantoss@users.noreply.github.com>
2020-03-21 16:03:08 -0300

    correção da lógica

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -28,18 +28,21 @@ void loop(){
                 }
 
                 if(c == '\n'){//se encontrar o '\n' eh o final da requisição
-                     if(readString.indexOf("?")>0){ //verifica se existe o caractere '?'
-                        if(readString.indexOf("ledParam=1")>0){ // verifica se a requisição foi para ligar o LED
+                     if(!(readString.indexOf("?")<0)){ //verifica se existe o caractere '?'
+                        if(readString.indexOf("ledParam=1")>=0){ // verifica se a requisição foi para ligar o LED
                               digitalWrite(ledPin, HIGH);
-                        }else if(readString.indexOf("ledParam=0")>0){ // verifica se a requisição foi para desligar o LED
+                        }else if(readString.indexOf("ledParam=0")>=0){ // verifica se a requisição foi para desligar o LED
                               digitalWrite(ledPin, LOW);
-                        }else if(readString.indexOf("ledStatus")>0){ // verifica se a requisição foi para checar o status do LED
+                        }else if(readString.indexOf("ledStatus")>=0){ // verifica se a requisição foi para checar o status do LED
+                         // client.println("<h1>");
                           client.println(digitalRead(ledPin));
+                        // client.println("</h1>");
                         }
-                        client.println("HTTP/1.1 200 OK"); //retorna um OK
+                    }
+
+                    client.println("HTTP/1.1 200 OK"); //retorna um OK
                     readString=""; //limpa a string para novas requisições
                     client.stop(); // desliga o cliente
-                    }
                 }
             }
         }
